2c1d05ceb3794a7499f52f9e6761704297172745
waxew <4622.ar@gmail.com>
2026-09-08 18:53:02 +0330

    Add initial Android activity

---
+++ app/src/main/kotlin/com/sourcebuild/template/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.sourcebuild.template
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
